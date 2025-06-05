1359927558cb4974f79bf90a104dffb8a4f6fe24
cryptaxe <cryptaxe@gmail.com>
2025-06-04 18:46:13 -0700

    Add unix build script

---
+++ build-unix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Build for unix with bench, fuzz, and tests disabled using all available cores
+# Also build Qt GUI 
+cmake -B build -DBUILD_GUI=ON -DBUILD_BENCH=OFF -DBUILD_FUZZ_BINARY=OFF -DBUILD_GUI_TESTS=OFF -DBUILD_TESTS=OFF
+cmake --build build -j $(nproc)
